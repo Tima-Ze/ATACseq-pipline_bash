c1e08fc68ad2daab129300745d0a09fa6434f456
Tima-Ze <timaz@zedat.fu-berlin.de>
2025-09-05 12:11:54 +0200

    final scripts up to duplicates removal

--- scripts/bowtie_map.sh
+++ scripts/bowtie_map.sh
@@ -3,14 +3,13 @@
 #SBATCH -D /data/scratch/timaz/atac
 #SBATCH --partition=nowick
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=125G
+#SBATCH --cpus-per-task=18
+#SBATCH --mem=64G
 #SBATCH -o report/bowtie.out
 #SBATCH -e report/bowtie.err
 #SBATCH --time=30-00:00:00
 
-
-mkdir -p results/bowtie2
+#mkdir -p results/bowtie2
 mkdir -p results/bam
 
 awk '{
@@ -19,7 +18,7 @@ awk '{
     sample     = $3
     print minus_val1, plus_val2, sample
 }' pairs_list.txt | 
-parallel --colsep ' ' -j 5 '
+parallel --colsep ' ' -j 10 '
     val1={1}
     val2={2}
     sample={3}
@@ -29,9 +28,8 @@ parallel --colsep ' ' -j 5 '
     bowtie2 -x ref/index \
             -1 results/trim_galore/"$val1" \
             -2 results/trim_galore/"$val2" \
-            -p 5 \
-            --very-sensitive \
+            -p 4 \
+            --very-sensitive -X 2000 \
     | samtools view -u - \
-    | samtools sort -o "results/bam/${sample}.bam" -
+    | samtools sort -@ 4 -o "results/bam/${sample}.bam" -
 '
-
--- scripts/filter_dup.sh
+++ scripts/filter_dup.sh
@@ -8,7 +8,7 @@
 #SBATCH -o report/filter_dup.out
 #SBATCH -e report/filter_dup.err
 #SBATCH --time=30-00:00:00
-#SBATCH --array=1-10
+#SBATCH --array=1-41%25
 
 mkdir -p results/bam_noDup
 mkdir -p results/bam_noDup/dup_metrics
@@ -17,14 +17,18 @@ bam=$(ls results/bam_noMT/*.bam | sed -n ${SLURM_ARRAY_TASK_ID}p)
 sample=$(basename "$bam" .fastq.gz_noMT.bam)
 
 # Define tmpdir
-SCRATCH_ROOT=${SCRATCH_ROOT:-/tmp} 
-tmpdir="${SCRATCH_ROOT}/${sample}"
+tmpdir="results/bam_noDup/tmp_${sample}"
 mkdir -p "$tmpdir"
+
+# Tell Spark/GATK to use it
+export SPARK_LOCAL_DIRS=$tmpdir
+export TMPDIR=$tmpdir
 
 samtools addreplacerg \
     -r ID:$sample -r LB:lib1 -r PL:ILLUMINA -r PU:unit1 -r SM:$sample \
     $bam -o "$tmpdir/${sample}_rg.bam"
-gatk MarkDuplicatesSpark \
+
+gatk --java-options "-Djava.io.tmpdir="$tmpdir"" MarkDuplicatesSpark \
     -I "$tmpdir/${sample}_rg.bam" \
     -O results/bam_noDup/${sample}_noDup.bam \
     -M results/bam_noDup/dup_metrics/${sample}_dup_metrics.txt \
@@ -32,3 +36,7 @@ gatk MarkDuplicatesSpark \
     --tmp-dir "${tmpdir}" \
     --spark-runner LOCAL \
     --spark-master local[8]
+	--conf spark.local.dir="$tmpdir"
+	--verbosity ERROR
+	
+rm -rf "$tmpdir"
