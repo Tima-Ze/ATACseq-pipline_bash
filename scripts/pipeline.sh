f971aeb58a131408a4b4205085e49e1bcda8a2d2
Tima-Ze <timaz@zedat.fu-berlin.de>
2025-10-28 15:28:29 +0100

    update pipeline

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,50 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Submit step 0: mkdir.sh
+# Submit mkdir.sh
 jid0=$(sbatch mkdir.sh | awk '{print $4}')
-echo "Submitted mkdir.sh as job $jid0"
 
-# Submit step 1: fastqc_fastq.sh
+# Submit fastqc_fastq.sh
 jid1=$(sbatch fastqc_fastq.sh | awk '{print $4}')
-echo "Submitted fastqc_fastq.sh as job $jid1"
 
-# Submit step 2: check_pairs.sh
-jid2=$(sbatch check_pairs.sh | awk '{print $4}')
-echo "Submitted check_pairs.sh as job $jid2"
+# Submit trim_galore.sh
+jid2=$(sbatch trim_galore.sh | awk '{print $4}')
 
-# Submit step 3: trim_galore.sh
-jid3=$(sbatch --dependency=afterok:$jid2 trim_galore.sh | awk '{print $4}')
-echo "Submitted trim_galore.sh as job $jid3"
+# Submit fastqc_trim_galore.sh
+jid3=$(sbatch --dependency=afterok:$jid2 fastqc_trim_galore.sh | awk '{print $4}')
 
-# Submit step 2: trimmomatic.sh
-#jid3=$(sbatch --dependency=afterok:$jid2 trimmomatic.sh | awk '{print $4}')
-#echo "Submitted trimmomatic.sh as job $jid3"
-
-# Submit step 2: cutadapt.sh
-#jid3=$(sbatch --dependency=afterok:$jid2 cutadapt.sh | awk '{print $4}')
-#echo "Submitted cutadapt.sh as job $jid3"
-
-# Submit step 4: fastqc_trim_galore.sh
-jid4=$(sbatch --dependency=afterok:$jid3 fastqc_trim_galore.sh | awk '{print $4}')
-echo "Submitted fastqc_trim_galore.sh as job $jid4"
-
-# Submit step 5: multiqcqc_trim_galore.sh
-jid5=$(sbatch --dependency=afterok:$jid4 multiqcqc_trim_galore.sh | awk '{print $4}')
-echo "Submitted multiqcqc_trim_galore.sh as job $jid5"
-
-# Submit step 6: bowtie_index.sh
-jid6=$(sbatch --dependency=afterok:$jid5 bowtie_index.sh | awk '{print $4}')
-echo "Submitted bowtie_index.sh as job $jid6"
-
-# Submit step 7: bowtie_map.sh
-jid7=$(sbatch --dependency=afterok:$jid6 bowtie_map.sh | awk '{print $4}')
-echo "Submitted bowtie_map.sh as job $jid7"
-
-# Submit step 8: filter_mito.sh
-jid8=$(sbatch --dependency=afterok:$jid7 filter_mito.sh | awk '{print $4}')
-echo "Submitted filter_mito.sh as job $jid8"
-
-# Submit step 9: filter_dup.sh
-jid9=$(sbatch --dependency=afterok:$jid8 filter_dup.sh | awk '{print $4}')
-echo "Submitted filter_dup.sh as job $jid9"
+#bowtie_map.sh
+jid4=$(sbatch --dependency=afterok:$jid3 bowtie_map.sh | awk '{print $4}')
